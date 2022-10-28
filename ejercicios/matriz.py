44077f1ccf8395667e4f26976fce6e578598cecc
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-28 15:55:47 +0200

    matriz iterativa

--- ejercicios/matriz.py
+++ ejercicios/matriz.py
@@ -17,4 +17,15 @@ def matriz_recursiva(matriz):
             suma += (-1)**(i%2)*matriz[0][i]*matriz_recursiva(aux)
         return suma
     
-print((matriz_recursiva([[3,5,8],[2,4,1],[2,0,7]])))
+print(matriz_recursiva([[3,5,8],[2,4,1],[2,0,7]]))  #3x3
+
+
+#matriz iterativa
+def matriz_iterativa(matriz):
+    suma = 0
+    for i in range(3): #3x3
+        suma = ((matriz[0][0] * matriz[1][1] * matriz[2][2])+(matriz[1][0] * matriz[2][1]*matriz[0][2]) +(matriz[0][1] * matriz[1][2] * matriz[2][0]))-(matriz[0][2] * matriz[1][1] * matriz[2][0] )-(matriz[0][1]*matriz[1][0]*matriz[2][2])-(matriz[1][2] * matriz[2][1] * matriz[0][0]) #calcular el determinante
+        
+    return suma
+
+print(matriz_iterativa([[3,5,8],[2,4,1],[2,0,7]]))
