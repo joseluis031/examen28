3eea75c33f3f045f4d3911a172c3281d7ad3018d
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-28 15:57:03 +0200

    ej 1

--- ejercicios/matriz.py
+++ ejercicios/matriz.py
@@ -3,7 +3,7 @@ import copy
 def matriz_recursiva(matriz):
     suma=0
     
-    if len(matriz) == 3 and len(matriz[0])==3:
+    if len(matriz) == 3 and len(matriz[0])==3: #columnas y filas
         suma = ((matriz[0][0] * matriz[1][1] * matriz[2][2])+(matriz[1][0] * matriz[2][1]*matriz[0][2]) +(matriz[0][1] * matriz[1][2] * matriz[2][0]))-(matriz[0][2] * matriz[1][1] * matriz[2][0] )-(matriz[0][1]*matriz[1][0]*matriz[2][2])-(matriz[1][2] * matriz[2][1] * matriz[0][0]) #calcular el determinante
         
         return suma
