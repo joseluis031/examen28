3a15cef311963a0d7ff7bef114a5f4be81d9e5a1
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-31 15:14:21 +0100

    mas funciones y corrijo fallos

--- ejercicios/naves.py
+++ ejercicios/naves.py
@@ -1,7 +1,7 @@
 import csv
 
 with open("Naves.csv","r") as file:
-    reader = csv.DictReader(file,delimeter=";") #leo csv
+    reader = csv.DictReader(file, delimiter=";") #leo csv
     lista = list(reader) #lo paso a lista para q las funciones sean mas faciles
     
 def ordenar():
@@ -12,4 +12,30 @@ def ordenar():
     print("Las naves ordenadas son: ",a)
     
 ordenar()
+
+def ordenar2():
+    orden = []
+    for i in lista:
+        orden.append(i["Largo"])
+    orden.sort(reverse=True)
+    print("Las naves ordenadas de mayor a menor por su largo son: ",orden)
+    
+ordenar2()
+
+def Halcon():
+    halcon = []
+    for i in lista:
+        if i["Nombre"] == "Halcon Milenario":
+            halcon.append(i)
+    print(halcon)  
+Halcon()
+
+def Estrella():
+    estrella = []
+    for i in lista:
+        if i["Nombre"] == "Estrella de la Muerte":
+            estrella.append(i)
+    print(estrella)  
+Estrella()
+
     
